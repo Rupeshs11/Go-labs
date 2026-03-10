7d416d3417df873936fea480f99ece374cb18ef2
rupeshs11 <rbsatale7@gmail.com>
2026-03-11 00:58:41 +0530

    while loop in go

--- learnings/05_Loops/while.go
+++ learnings/05_Loops/while.go
@@ -1,23 +1,23 @@
 package main
 
-//import "fmt"
+import "fmt"
 
-// there is only for loop in go
-// but we can implement the while loop using for
-// func main(){
-// 	//there is no while keyword in golang
+there is only for loop in go
+but we can implement the while loop using for
+func main(){
+	//there is no while keyword in golang
 
-// 	//while loop
-// 	i:=1
+	//while loop
+	i:=1
 
-// 	for i<=3 {
-// 		fmt.Println(i)
-// 		i++
-// 	}
+	for i<=3 {
+		fmt.Println(i)
+		i++
+	}
 
-// 	// infinite loop
-// 	// for { }
+	// infinite loop
+	// for { }
 	
 
-// }
+}
 
