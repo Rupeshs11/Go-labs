94433374b57d4713d8020d84fef2befafa8f93eb
rupeshs11 <rbsatale7@gmail.com>
2026-03-15 10:53:06 +0530

    stack using geenrics in golang

---
+++ learnings/19_generics/stack.go
@@ -0,0 +1,15 @@
+package main
+
+// import "fmt"
+
+// type stack[T any] struct{
+// 	elements []T
+// }
+
+// func main(){
+// 	mystack:= stack[int]{
+// 		elements: []int{1,2,3},
+// 	}
+
+// 	fmt.Println(mystack)
+// }
