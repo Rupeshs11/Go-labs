6edbf0dd721ed38eda01e1fbdaf725fdc8c2546d
rupeshs11 <rbsatale7@gmail.com>
2026-03-19 21:57:33 +0530

    reading a folder or directory in golang

---
+++ learnings/23_File-Handling/ReadFolder.go
@@ -0,0 +1,23 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"os"
+// )
+
+// func main() {
+
+// 	dir, err := os.Open(".")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	defer dir.Close()
+
+// 	fileInfo, err := dir.ReadDir(3)
+
+// 	for _, fi := range fileInfo {
+// 		fmt.Println(fi.Name())
+// 	}
+
+// }
