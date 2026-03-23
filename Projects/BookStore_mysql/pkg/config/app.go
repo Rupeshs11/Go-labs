2b3a96a2998f0e53e16644e43077ab2889e67759
rupeshs11 <rbsatale7@gmail.com>
2026-03-23 23:16:27 +0530

    packages for Bookstore

---
+++ Projects/BookStore_mysql/pkg/config/app.go
@@ -0,0 +1,25 @@
+package config
+
+import(
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db * gorm.DB
+)
+
+func Connect(){
+	fmt.Println("Connecting to MySQL database...")
+	d, err := gorm.Open("mysql","root:Technopath256@tcp(127.0.0.1:3306)/go_bookstore?charset=utf8mb4&parseTime=True&loc=Local&timeout=10s")
+	if err !=nil{
+		panic(err)
+	}
+	fmt.Println("Successfully connected to database!")
+	db = d
+}
+
+func GetDB() *gorm.DB{
+	return db
+}
