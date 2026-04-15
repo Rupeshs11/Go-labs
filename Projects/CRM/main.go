a8973a77526636fb34ca8b52a307e8ba9fbfa720
Rupeshs11 <rbsatale7@gmail.com>
2026-04-15 22:18:45 +0530

    main file for CRM

---
+++ Projects/CRM/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/rupeshs11/CRM/database"
+	"github.com/rupeshs11/CRM/lead"
+	"github.com/gofiber/fiber"
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("/api/v1/lead/:id", lead.GetLead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead/:id", lead.DeleteLead)
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open(sqlite.Open("leads.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection opened to database")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database Migrated")
+}
+
+func main() {
+	app := fiber.New()
+	initDatabase()
+	setupRoutes(app)
+	app.Listen(3000)
+	sqlDB, _ := database.DBConn.DB()
+	if sqlDB != nil {
+		defer sqlDB.Close()
+	}
+}
